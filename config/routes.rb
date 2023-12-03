cdb3eefe8fd69a0af6cb5eaf4e611754307f0423
Jun-Araki <jun7772006@gmail.com>
2023-12-04 07:28:33 +0900

    generate csv_import

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: :index
+  get 'import/create'
+  resources :users, only: :index do
+    collection { resources :import, only: :create, controller: :import }
+  end
 end
