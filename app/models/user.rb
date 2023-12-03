a32443bff4a59e7ec8cc3ab36de4e7caa70ae7d7
Jun-Araki <jun7772006@gmail.com>
2023-12-04 08:09:31 +0900

    import csv

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  CSV_COLUMNS = %w[name age height].freeze
+
+  def self.import_csv(file)
+    list = []
+
+    User.transaction do
+      CSV.foreach(file.path, headers: true) do |row|
+        list << row.to_h.slice(*CSV_COLUMNS)
+      end
+      User.import!(list)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'import/create'
   resources :users, only: :index do
     collection { resources :import, only: :create, controller: :import }
   end
